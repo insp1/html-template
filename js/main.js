5e9d3e606bdf268d28aadc2445a4cc9cd592b515
Aleksey Vavilov <alexeyvavilov1992@yandex.ru>
2017-07-19 11:10:53 +0500

    Font + form
    Add Roboto font and ajax-form + mailer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+// Отправка заявки 
+$(document).ready(function() {
+	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+		if (document.form.name.value == '' || document.form.phone.value == '' ) {
+			valid = false;
+			return valid;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mail/mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$('.js-overlay-thank-you').fadeIn();
+			$(this).find('input').val('');
+			$('#form').trigger('reset');
+		});
+		return false;
+	});
+});
+
+// Закрыть попап «спасибо»
+$('.js-close-thank-you').click(function() { // по клику на крестик
+	$('.js-overlay-thank-you').fadeOut();
+});
+
+$(document).mouseup(function (e) { // по клику вне попапа
+    var popup = $('.popup');
+    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+        $('.js-overlay-thank-you').fadeOut();
+    }
+});
+
 // Маска ввода номера телефона (плагин maskedinput)
 $(function($){
 	$('[name="phone"]').mask("+7(999) 999-9999");
---
+++ mail/mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "alexeyvavilov1992@yandex.ru";
+$siteName = "HTML-шаблон";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Имя: $name \nТелефон: $phone";
+
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
